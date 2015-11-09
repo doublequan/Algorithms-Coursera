40bfab4df36e49de0f3e2091505d5c40d23a5949
doublequan <msa_quanquan@163.com>
2015-11-09 22:50:24 +0800

    outcast.java

---
+++ Week7/Outcast.java
@@ -0,0 +1,54 @@
+/****************************************************************************
+  *  the programming assignment of Algorithms, Part II 
+  *  Week1 Part II
+  *  author: Bill Quan  
+  *  Last edited: 20151109
+  *  Outcast.java
+  ****************************************************************************/
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+
+public class Outcast {
+    private WordNet wn;
+    
+    public Outcast(WordNet wordnet)         // constructor takes a WordNet object
+    {
+        wn = wordnet;
+    }
+    public String outcast(String[] nouns)   // given an array of WordNet nouns, return an outcast
+    {
+        int dist[] = new int[nouns.length];
+        for (int i = 0; i < nouns.length; i++)
+            for (int j = 0; j < nouns.length; j++)
+        {
+            if (i != j)
+            {
+                dist[i] += wn.distance(nouns[i], nouns[j]);
+            }
+        }
+        int maxid = 0;
+        for (int i : dist)
+        {
+            if (dist[maxid] < dist[i])
+                maxid = i;
+        }
+        return nouns[maxid];
+    }
+    
+    
+    
+    public static void main(String[] args) 
+    {
+        WordNet wordnet = new WordNet(args[0], args[1]);
+        Outcast outcast = new Outcast(wordnet);
+        for (int t = 2; t < args.length; t++) 
+        {
+            In in = new In(args[t]);
+            String[] nouns = in.readAllStrings();
+            StdOut.println(args[t] + ": " + outcast.outcast(nouns));
+        }
+    }
+}
